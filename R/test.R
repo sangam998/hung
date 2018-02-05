9a0e4307efefcbc7855296c4a2267806ccfbb2da
sangam998 <36173099+sangam998@users.noreply.github.com>
2018-02-05 13:09:18 -0800

    Initial commit

---
+++ R/test.R
@@ -0,0 +1,17 @@
+#' A Cat Function
+#'
+#' This function allows you to express your love of catssssssss.
+#' @param love Do you love cats? Defaults to TRUE.
+#' @keywords cats
+#' @export
+#' @examples
+#' cat_function()
+
+cat_functionn <- function(love=TRUE){
+    if(love==TRUE){
+        print("I love cats!")
+    }
+    else {
+        print("I am not a cool person.")
+    }
+}
